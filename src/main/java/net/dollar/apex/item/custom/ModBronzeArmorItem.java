16595159416d1a15468359235dfa208c19b1d701
dollar <tmg1999@yahoo.com>
2024-10-12 14:41:15 -0500

    Update to version 1.1.1
    Fix missing enchantments for Bows, Crossbows, and Armors;

--- src/main/java/net/dollar/apex/item/custom/ModBronzeArmorItem.java
+++ src/main/java/net/dollar/apex/item/custom/ModBronzeArmorItem.java
@@ -1,51 +1,17 @@
-package net.dollar.apex.item.custom.tungstencarbide;
+package net.dollar.apex.item.custom;
 
-import net.dollar.apex.item.ModItems;
-import net.dollar.apex.util.IFullSetEffectArmor;
-import net.dollar.apex.util.ModItemUtils;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
-import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
-
-public class ModTungstenCarbideArmorItem extends ArmorItem implements IFullSetEffectArmor {
-    public ModTungstenCarbideArmorItem(ArmorMaterial material, Type type, Item.Properties properties) {
+public class ModBronzeArmorItem extends ArmorItem {
+    public ModBronzeArmorItem(ArmorMaterial material, Type type, Properties properties) {
         super(material, type, properties);
     }
 
 
-
-    /**
-     * IFullSetEffectArmor interface method that prevents an effect from being applied if a full set is worn.
-     * @param effect Effect trying to be applied
-     * @return Whether the effect can be applied to this armor's wearer
-     */
-    @Override
-    public boolean canReceiveEffect(MobEffect effect, LivingEntity wearer) {
-        //Can receive effect UNLESS full set and effect is weakness.
-        boolean isFullSet = false;
-
-        //Check for correct equipment, then set isFullSet accordingly
-        if (wearer instanceof Player player) {
-            boolean hasHelm = player.getItemBySlot(EquipmentSlot.HEAD).getItem() == ModItems.TUNGSTEN_CARBIDE_HELMET.get();
-            boolean hasChest = player.getItemBySlot(EquipmentSlot.CHEST).getItem() == ModItems.TUNGSTEN_CARBIDE_CHESTPLATE.get();
-            boolean hasLegs = player.getItemBySlot(EquipmentSlot.LEGS).getItem() == ModItems.TUNGSTEN_CARBIDE_LEGGINGS.get();
-            boolean hasBoots = player.getItemBySlot(EquipmentSlot.FEET).getItem() == ModItems.TUNGSTEN_CARBIDE_BOOTS.get();
-            isFullSet = hasHelm && hasChest && hasLegs && hasBoots;
-        }
-
-        return !(isFullSet && (effect == MobEffects.MOVEMENT_SLOWDOWN || effect == MobEffects.LEVITATION));
-    }
 
     /**
      * Allow or deny specific enchantment application to this Item. For Battleaxes, allow all
@@ -94,17 +60,5 @@ public class ModTungstenCarbideArmorItem extends ArmorItem implements IFullSetEf
                     enchantment == Enchantments.MENDING);
         }
         return false;   //Return false if none of the types above.
-    }
-
-    /**
-     * Appends text to the Item's hover tooltip.
-     * @param stack ItemStack corresponding to this item
-     * @param level Relevant level
-     * @param tooltip List of tooltip texts to render
-     * @param flag TooltipFlag determining data like simple or advanced
-     */
-    @Override
-    public void appendHoverText(@NotNull ItemStack stack, @Nullable Level level, @NotNull List<Component> tooltip, @NotNull TooltipFlag flag) {
-        ModItemUtils.appendCobaltSteelEquipmentTooltip(tooltip, ModItemUtils.EquipmentType.ARMOR);
     }
 }
--- src/main/java/net/dollar/apex/item/custom/infusedgemstone/ModInfusedGemstoneArmorItem.java
+++ src/main/java/net/dollar/apex/item/custom/infusedgemstone/ModInfusedGemstoneArmorItem.java
@@ -10,6 +10,8 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -45,7 +47,54 @@ public class ModInfusedGemstoneArmorItem extends ArmorItem implements IFullSetEf
         return !(isFullSet && (effect == MobEffects.WITHER || effect == MobEffects.POISON));
     }
 
-
+    /**
+     * Allow or deny specific enchantment application to this Item. For Battleaxes, allow all
+     *  vanilla weapon enchantments except Sweeping Edge.
+     * @param stack The ItemStack attempting to be enchanted (this)
+     * @param enchantment The Enchantment attempting to be applied
+     * @return Whether the enchantment is allowed
+     */
+    @Override
+    public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+        if (this.type == Type.HELMET) {
+            return (enchantment == Enchantments.AQUA_AFFINITY ||
+                    enchantment == Enchantments.BLAST_PROTECTION ||
+                    enchantment == Enchantments.FIRE_PROTECTION ||
+                    enchantment == Enchantments.PROJECTILE_PROTECTION ||
+                    enchantment == Enchantments.ALL_DAMAGE_PROTECTION ||
+                    enchantment == Enchantments.RESPIRATION ||
+                    enchantment == Enchantments.UNBREAKING ||
+                    enchantment == Enchantments.MENDING);
+        } else if (this.type == Type.CHESTPLATE) {
+            return (enchantment == Enchantments.BLAST_PROTECTION ||
+                    enchantment == Enchantments.FIRE_PROTECTION ||
+                    enchantment == Enchantments.PROJECTILE_PROTECTION ||
+                    enchantment == Enchantments.ALL_DAMAGE_PROTECTION ||
+                    enchantment == Enchantments.THORNS ||
+                    enchantment == Enchantments.UNBREAKING ||
+                    enchantment == Enchantments.MENDING);
+        } else if (this.type == Type.LEGGINGS) {
+            return (enchantment == Enchantments.BLAST_PROTECTION ||
+                    enchantment == Enchantments.FIRE_PROTECTION ||
+                    enchantment == Enchantments.PROJECTILE_PROTECTION ||
+                    enchantment == Enchantments.ALL_DAMAGE_PROTECTION ||
+                    enchantment == Enchantments.SWIFT_SNEAK ||
+                    enchantment == Enchantments.UNBREAKING ||
+                    enchantment == Enchantments.MENDING);
+        } else if (this.type == Type.BOOTS) {
+            return (enchantment == Enchantments.BLAST_PROTECTION ||
+                    enchantment == Enchantments.DEPTH_STRIDER ||
+                    enchantment == Enchantments.FALL_PROTECTION ||
+                    enchantment == Enchantments.FROST_WALKER ||
+                    enchantment == Enchantments.FIRE_PROTECTION ||
+                    enchantment == Enchantments.PROJECTILE_PROTECTION ||
+                    enchantment == Enchantments.ALL_DAMAGE_PROTECTION ||
+                    enchantment == Enchantments.SOUL_SPEED ||
+                    enchantment == Enchantments.UNBREAKING ||
+                    enchantment == Enchantments.MENDING);
+        }
+        return false;   //Return false if none of the types above.
+    }
 
     /**
      * Appends text to the Item's hover tooltip.
