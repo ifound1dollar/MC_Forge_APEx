4cbf06e3dda648e274afe88a7bd5d1390dc0c785
dollar <tmg1999@yahoo.com>
2024-10-12 13:50:31 -0500

    Update to version 1.1.0
    Fix incorrect call to super.onHit() instead of super.onHitEntity() in Cobalt Steel and Tungsten Carbide arrow entity classes;

--- src/main/java/net/dollar/apex/item/custom/arrow/CobaltSteelArrowEntity.java
+++ src/main/java/net/dollar/apex/item/custom/arrow/CobaltSteelArrowEntity.java
@@ -43,7 +43,7 @@ public class CobaltSteelArrowEntity extends Arrow {
      */
     @Override
     protected void onHitEntity(@NotNull EntityHitResult hitResult) {
-        super.onHit(hitResult);
+        super.onHitEntity(hitResult);
 
         //Only if hit Entity is a LivingEntity.
         if (hitResult.getEntity() instanceof LivingEntity livingEntity) {
